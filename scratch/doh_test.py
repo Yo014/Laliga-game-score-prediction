03facc3914c059ab127efdb997419a4aaf667b82
Santo Mukiza <emukisant@gmail.com>
2026-05-28 17:46:24 -0400

    chore: remove all project source files, datasets, and model artifacts

--- scratch/doh_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-doh_url = "https://dns.google/resolve?name=understat.com&type=A"
-try:
-    resp = requests.get(doh_url)
-    print("DoH response:", resp.json())
-except Exception as e:
-    print("Error:", e)
